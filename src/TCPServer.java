f5519ae6dd6ae763f683c178092a285235f819f7
sahilappay <sahil.appayev@mail.ru>
2020-01-30 16:46:07 +0400

    new commit

--- src/TCPServer.java
+++ src/TCPServer.java
@@ -12,14 +12,15 @@ public class TCPServer {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter file name or file path:");
         String fileName = sc.nextLine();
-        connectServerForFile(fileName);
+        connectServerForFileNew(fileName);
     }
 
     public static void connectServer() throws Exception{
-        ServerSocket ourFirstSocket = new ServerSocket(6789);
+        ServerSocket ourFirstSocket = new ServerSocket(6789);// localhost ve ya 127.0.0.1
         while (true){
             Socket connectionSocket = ourFirstSocket.accept();
-            InputStreamReader isr = new InputStreamReader(connectionSocket.getInputStream());// melumati qebul edir
+            InputStream is = connectionSocket.getInputStream();
+            InputStreamReader isr = new InputStreamReader(is);// melumati qebul edir
             BufferedReader bfr = new BufferedReader(isr);
             String clientMessage = bfr.readLine();
             System.out.println(clientMessage);
@@ -56,5 +57,32 @@ public class TCPServer {
 
     public static void writeUsingPath(String data,String filePath) {
         writeUsingPath(data,filePath, false);
+    }
+    
+    ////////////////NEW METHODS\\\\\\\\\\\\\\\\\
+    
+    
+     public static void connectServerForFileNew(String fileName) throws Exception{
+        ServerSocket ourFirstSocket = new ServerSocket(6789);// localhost ve ya 127.0.0.1
+        while (true){
+            Socket connectionSocket = ourFirstSocket.accept();
+            InputStream is = connectionSocket.getInputStream();
+            DataInputStream dis = new DataInputStream(is);
+            int length = dis.readInt();
+            byte[] bytes = new byte[length];
+            dis.readFully(bytes);
+            writeBytes(fileName, bytes);
+            System.out.println("File accepted!");
+        }
+    }
+    
+      public static void writeBytes(String fileName, byte[] data) throws Exception {
+        File file = new File(fileName);
+        try (FileOutputStream fos = new FileOutputStream(file)) {
+//            byte[] bytes = object.toString().getBytes();
+            fos.write(data);
+            fos.flush(); // topladigi melumatlari yazir ve prosesi tesdiqleyir
+        }
+//        System.out.println("Done");
     }
 }
